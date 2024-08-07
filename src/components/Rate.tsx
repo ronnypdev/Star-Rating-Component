f9880717fd15909a198e1fddbb016853de395ffb
Ronny Perez <ronnypdev@gmail.com>
2024-08-07 18:28:25 -0400

    implemented start rating icons and fixed component height

--- src/components/Rate.tsx
+++ src/components/Rate.tsx
@@ -3,10 +3,16 @@ import Star from "./Star"
 export default function Rate() {
   return (
     <>
-      <article className="w-[647px] h-[266px] flex flex-col justify-center items-center p-16 gap-8 bg-white rounded-2xl shadow-lg">
+      <article className="w-[647px] h-[310px] flex flex-col justify-center items-center p-16 gap-8 bg-white rounded-2xl shadow-lg">
         <h3 className="text-darkBlue text-center font-Merriweather text-2xl leading-normal font-bold">How many stars would you give to our Online Code Editor?</h3>
-        <Star />
-        <p className="font-Poppins text-lg font-normal leading-normal text-darkGray">We apologize for the inconvenience you experienced. We appreciate your feedback and would like to work with you to address any issues.</p>
+        <div className="flex justify-between items-center w-full max-w-full">
+          <Star />
+          <Star />
+          <Star />
+          <Star />
+          <Star />
+        </div>
+        <p className="font-Poppins text-sm font-normal leading-normal text-darkGray">We apologize for the inconvenience you experienced. We appreciate your feedback and would like to work with you to address any issues.</p>
       </article>
     </>
   )
--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -1,9 +1,10 @@
+import startEmpty from "../assets/images/star_empty.svg";
+import startFilled from "../assets/images/star_filled.svg";
+
 export default function Star() {
   return (
     <>
-      <div>
-        Star
-      </div>
+      <img className="cursor-pointer" src={startFilled} alt="star icon filled" width={80} height={80} />
     </>
   )
 }
