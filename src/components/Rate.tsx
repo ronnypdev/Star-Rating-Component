19ffc18292540f969755aebdec733515f7796360
Ronny Perez <ronnypdev@gmail.com>
2024-08-08 15:34:23 -0400

    Starelement TypeScript Bug

--- src/components/Rate.tsx
+++ src/components/Rate.tsx
@@ -1,16 +1,21 @@
 import Star from "./Star"
 
+
 export default function Rate() {
+
+  const starElement = [...Array(6)].forEach(() => (
+    <Star
+      startAltTextFilled="Star Icon Fill Yellow"
+      startAltTextEmpty="Star Icon Empty Grey"
+    />
+  ));
+
   return (
     <>
       <article className="w-[647px] h-[310px] flex flex-col justify-center items-center p-16 gap-8 bg-white rounded-2xl shadow-lg">
         <h3 className="text-darkBlue text-center font-Merriweather text-2xl leading-normal font-bold">How many stars would you give to our Online Code Editor?</h3>
         <div className="flex justify-between items-center w-full max-w-full">
-          <Star />
-          <Star />
-          <Star />
-          <Star />
-          <Star />
+         {starElement}
         </div>
         <p className="font-Poppins text-sm font-normal leading-normal text-darkGray">We apologize for the inconvenience you experienced. We appreciate your feedback and would like to work with you to address any issues.</p>
       </article>
--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -1,10 +1,24 @@
-import startEmpty from "../assets/images/star_empty.svg";
-import startFilled from "../assets/images/star_filled.svg";
+import { useState } from "react";
+import starEmpty from "../assets/images/star_empty.svg";
+import starFilled from "../assets/images/star_filled.svg";
 
-export default function Star() {
+type Star = {
+  startAltTextFilled: string,
+  startAltTextEmpty: string,
+}
+
+export default function Star(
+  { startAltTextFilled,
+    startAltTextEmpty }: Star) {
+  const [star, setStar] = useState<boolean>(false)
+
+  function handleStarToggle(): void {
+    setStar(prevStar => !prevStar)
+  }
+
   return (
     <>
-      <img className="cursor-pointer" src={startFilled} alt="star icon filled" width={80} height={80} />
+      <img className="cursor-pointer" src={star ? starFilled : starEmpty} onClick={handleStarToggle} alt={star ? startAltTextFilled : startAltTextEmpty} width={80} height={80} />
     </>
   )
 }
