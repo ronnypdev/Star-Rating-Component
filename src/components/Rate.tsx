de1f5d3cd5770bca3505fb4ce6b05d447d53a653
Ronny Perez <ronnypdev@gmail.com>
2024-08-14 13:49:21 -0400

    Refactored Rate component deleted Star component

--- src/components/Rate.tsx
+++ src/components/Rate.tsx
@@ -1,12 +1,46 @@
-import Star from "./Star"
+import { useState } from "react";
+import starEmpty from "../assets/images/star_empty.svg";
+import starFilled from "../assets/images/star_filled.svg";
 
-export default function Rate() {
+type Rate = {
+  rating: number,
+  ratingQuestion: string,
+  startAltTextFilled: string,
+  startAltTextEmpty: string,
+}
+
+export default function Rate({
+  rating,
+  ratingQuestion,
+  startAltTextFilled,
+  startAltTextEmpty
+}: Rate) {
+  const [star, setStar] = useState<boolean>(false);
+  const [hoverStar, setHoverStar] = useState<boolean>(false);
+
+  function handleStarToggle(): void {
+    setStar(prevStar => !prevStar);
+  }
+
+  function handleFilledStarHover(): void {
+    setHoverStar(true);
+  }
+
+  function handleEmptyStarHover(): void {
+    setHoverStar(false)
+  }
+
 
   const starElement: JSX.Element[] = [...Array(5)].map((_, index) => (
-    <Star
+    <img className="cursor-pointer"
       key={index}
-      startAltTextFilled="Star Icon Fill Yellow"
-      startAltTextEmpty="Star Icon Empty Grey"
+      src={star || hoverStar ? starFilled : starEmpty}
+      onClick={handleStarToggle}
+      onMouseEnter={handleFilledStarHover}
+      onMouseLeave={handleEmptyStarHover}
+      alt={star || hoverStar ? startAltTextFilled : startAltTextEmpty}
+      width={80}
+      height={80}
     />
   ));
 
@@ -15,7 +49,9 @@ export default function Rate() {
   return (
     <>
       <article className="w-[647px] h-[310px] flex flex-col justify-center items-center p-16 gap-8 bg-white rounded-2xl shadow-lg">
-        <h3 className="text-darkBlue text-center font-Merriweather text-2xl leading-normal font-bold">How many stars would you give to our Online Code Editor?</h3>
+        <h3 className="text-darkBlue text-center font-Merriweather text-2xl leading-normal font-bold">
+          How many stars would you give to our Online Code Editor?
+        </h3>
         <div className="flex justify-between items-center w-full max-w-full">{starElement}</div>
         <p className="font-Poppins text-sm font-normal leading-normal text-darkGray">We apologize for the inconvenience you experienced. We appreciate your feedback and would like to work with you to address any issues.</p>
       </article>
