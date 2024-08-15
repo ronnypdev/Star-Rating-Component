a2a4c5dd9082a1ca9df9e8d4102b4b800be5af7b
Ronny Perez <ronnypdev@gmail.com>
2024-08-15 11:58:18 -0400

    Hover and Clicking star bug ongoing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import Rate from "./components/Rate"
 function App() {
   return (
     <main className="h-full max-container flex justify-center items-center">
-      <Rate />
+      <Rate
+        ratingQuestion="How many stars would you give to our Online Code Editor?"
+        startAltTextFilled="Yellow Star Icon Filled"
+        startAltTextEmpty="Empty Star Icon Empty"
+      />
     </main>
   )
 }
--- src/components/Rate.tsx
+++ src/components/Rate.tsx
@@ -1,58 +1,52 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import starEmpty from "../assets/images/star_empty.svg";
 import starFilled from "../assets/images/star_filled.svg";
-import { nanoid } from 'nanoid';
 
 type Rate = {
-  rating: number,
   ratingQuestion: string,
   startAltTextFilled: string,
   startAltTextEmpty: string,
 }
 
 export default function Rate({
-  rating,
   ratingQuestion,
   startAltTextFilled,
   startAltTextEmpty
 }: Rate) {
-  const [star, setStar] = useState<boolean>(false);
-  const [hoverStar, setHoverStar] = useState<boolean>(false);
-  // const starImgRef = useRef(null)
+  // const [star, setStar] = useState<boolean>(false);
+  const [rating, setRating] = useState<null>(null);
+  const [hoverStar, setHoverStar] = useState<null>(null);
+  const [starNumber, setStarNumber] = useState<number>(5)
 
   function handleStarToggle(): void {
-    setStar(prevStar => !prevStar);
+    // setStar(prevStar => !prevStar);
   }
 
-  function handleFilledStarHover(): void {
-    setHoverStar(true);
-  }
+  const starElement: JSX.Element[] = [...Array(starNumber)].map((star, index) => {
+    const currentRating = index + 1;
+    return (
+      <>
+        <img className="cursor-pointer"
+          key={index}
+          src={currentRating <= (hoverStar || rating) ? starFilled : starEmpty}
+          onClick={handleStarToggle}
+          alt={currentRating <= (hoverStar || rating) ? startAltTextFilled : startAltTextEmpty}
+          onMouseEnter={() => setHoverStar(currentRating)}
+          onMouseLeave={() => setHoverStar(null)}
+          width={80}
+          height={80}
+        />
+      </>
+    )
+  });
 
-  function handleEmptyStarHover(): void {
-    setHoverStar(false)
-  }
-
-
-  const starElement: JSX.Element[] = [...Array(5)].map((_, index) => (
-    <img className="cursor-pointer"
-      key={index}
-      src={star ? starFilled : starEmpty}
-      onClick={handleStarToggle}
-      onMouseEnter={handleFilledStarHover}
-      onMouseLeave={handleEmptyStarHover}
-      alt={star ? startAltTextFilled : startAltTextEmpty}
-      width={80}
-      height={80}
-    />
-  ));
-
-  console.log("starElement: ", starElement.length)
+  // console.log("starElement: ", starElement.length)
 
   return (
     <>
       <article className="w-[647px] h-[310px] flex flex-col justify-center items-center p-16 gap-8 bg-white rounded-2xl shadow-lg">
         <h3 className="text-darkBlue text-center font-Merriweather text-2xl leading-normal font-bold">
-          How many stars would you give to our Online Code Editor?
+          {ratingQuestion}
         </h3>
         <div className="flex justify-between items-center w-full max-w-full">{starElement}</div>
         <p className="font-Poppins text-sm font-normal leading-normal text-darkGray">We apologize for the inconvenience you experienced. We appreciate your feedback and would like to work with you to address any issues.</p>
