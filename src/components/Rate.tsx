52b5236ab521360f6114871711d76da9c2c935ba
Ronny Perez <ronnypdev@gmail.com>
2024-08-19 10:55:37 -0400

    Implemented hover and click functions

--- src/components/Rate.tsx
+++ src/components/Rate.tsx
@@ -13,24 +13,20 @@ export default function Rate({
   startAltTextFilled,
   startAltTextEmpty
 }: Rate) {
-  // const [star, setStar] = useState<boolean>(false);
-  const [rating, setRating] = useState<null>(null);
-  const [hoverStar, setHoverStar] = useState<null>(null);
-  const [starNumber, setStarNumber] = useState<number>(5)
+  const [rating, setRating] = useState<number | null>(null);
+  const [hoverStar, setHoverStar] = useState<number | null>(null);
 
-  function handleStarToggle(): void {
-    // setStar(prevStar => !prevStar);
-  }
 
-  const starElement: JSX.Element[] = [...Array(starNumber)].map((star, index) => {
+  const starElement: JSX.Element[] = [...Array(5)].map((_, index) => {
     const currentRating = index + 1;
+    console.log("currentRating", currentRating)
     return (
       <>
         <img className="cursor-pointer"
           key={index}
-          src={currentRating <= (hoverStar || rating) ? starFilled : starEmpty}
-          onClick={handleStarToggle}
-          alt={currentRating <= (hoverStar || rating) ? startAltTextFilled : startAltTextEmpty}
+          src={currentRating <= (hoverStar! || rating!) ? starFilled : starEmpty}
+          onClick={() => setRating(currentRating)}
+          alt={currentRating <= (hoverStar! || rating!) ? startAltTextFilled : startAltTextEmpty}
           onMouseEnter={() => setHoverStar(currentRating)}
           onMouseLeave={() => setHoverStar(null)}
           width={80}
@@ -40,16 +36,18 @@ export default function Rate({
     )
   });
 
-  // console.log("starElement: ", starElement.length)
-
   return (
     <>
       <article className="w-[647px] h-[310px] flex flex-col justify-center items-center p-16 gap-8 bg-white rounded-2xl shadow-lg">
         <h3 className="text-darkBlue text-center font-Merriweather text-2xl leading-normal font-bold">
           {ratingQuestion}
         </h3>
-        <div className="flex justify-between items-center w-full max-w-full">{starElement}</div>
-        <p className="font-Poppins text-sm font-normal leading-normal text-darkGray">We apologize for the inconvenience you experienced. We appreciate your feedback and would like to work with you to address any issues.</p>
+        <div className="flex justify-between items-center w-full max-w-full">
+          {starElement}
+        </div>
+        <p className="font-Poppins text-sm font-normal leading-normal text-darkGray">
+          We apologize for the inconvenience you experienced. We appreciate your feedback and would like to work with you to address any issues.
+        </p>
       </article>
     </>
   )
