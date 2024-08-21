49018bf7386c5657eacb29bad2e2c474ff82b466
Ronny Perez <ronnypdev@gmail.com>
2024-08-21 11:17:23 -0400

    Implemented change rating text and fixed TS bugs

--- src/components/Rate.tsx
+++ src/components/Rate.tsx
@@ -35,6 +35,23 @@ export default function Rate({
     )
   });
 
+  function changeRatingText(ratingNumber: number | null): string {
+    switch (ratingNumber) {
+        case 1:
+          return "We're sorry to hear that you had a bad experience. We would like to learn more about what happened and how we can make things right.";
+        case 2:
+          return "We apologize for the inconvenience you experienced. We appreciate your feedback and would like to work with you to address any issues.";
+        case 3:
+          return "Thank you for your feedback. We're sorry to hear that your experience wasn't perfect. We would love to hear more about your concerns to see how we can improve.";
+        case 4:
+          return "Thank you for your positive feedback! We're glad to know that you had a great experience and we appreciate your support.";
+        case 5:
+          return "Excellent! We're thrilled to hear you had such a positive experience. Thank you for choosing our platform.";
+        default:
+          return "";
+    }
+  }
+
   return (
     <>
       <article className="w-[647px] h-[310px] flex flex-col justify-center items-center p-16 gap-8 bg-white rounded-2xl shadow-lg">
@@ -45,8 +62,7 @@ export default function Rate({
           {starElement}
         </div>
         <p className="font-Poppins text-sm font-normal leading-normal text-darkGray">
-          We apologize for the inconvenience you experienced. We appreciate your feedback and would like to work with you to address any issues.
-          {rating}
+          {changeRatingText(rating)}
         </p>
       </article>
     </>
